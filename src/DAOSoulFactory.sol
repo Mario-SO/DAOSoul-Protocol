b27997637b998c344697c364d7ce6820ba8c6f92
Mario-SO <gh_mario.proclaim@slmail.me>
2022-08-24 22:21:02 +0200

    First approach to Factory pattern finished

---
+++ src/DAOSoulFactory.sol
@@ -0,0 +1,26 @@
+pragma solidity >=0.8.0;
+// SPDX-License-Identifier: MIT
+
+import "./DAOSoul.sol";
+import "@optionality/contracts/CloneFactory.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract DAOSoulFactory is Ownable, CloneFactory {
+    address public libraryAddress;
+
+    event SoulCreated(address newFoundation);
+
+    function SoulFactory(address _libraryAddress) public {
+        libraryAddress = _libraryAddress;
+    }
+
+    function setLibraryAddress(address _libraryAddress) public onlyOwner {
+        libraryAddress = _libraryAddress;
+    }
+
+    function createSoul(string _name) public onlyOwner {
+        address clone = createClone(libraryAddress);
+        DAOSoul(clone).init(_name);
+        SoulCreated(clone);
+    }
+}
