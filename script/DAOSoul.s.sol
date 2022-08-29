667c45730226411f3ae4a479cbefff4fb32948fa
Mario-SO <gh_mario.proclaim@slmail.me>
2022-08-29 10:17:35 +0200

    Deployment script

--- script/DAOSoul.s.sol
+++ script/DAOSoul.s.sol
@@ -3,10 +3,16 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
 
+import {DAOSoul} from "src/DAOSoul.sol"
+import {DAOSoulFactory} from "src/DAOSoulFactory.sol"
+
 contract CounterScript is Script {
     function setUp() public {}
 
     function run() public {
-        vm.broadcast();
+        vm.startBroadcast();
+        soul = new DAOSoul();
+        factory = new DAOSoulFactory()
+        vm.stopBroadcast();
     }
 }
